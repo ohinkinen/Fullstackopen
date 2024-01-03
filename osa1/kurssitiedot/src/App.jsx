41e810203c288cb54fb94943d344bbfde9054fa4
Olli-Heikki Inkinen <inkinenolliheikki@gmail.com>
2024-01-03 11:43:34 +0200

    1.3 - 1.5: kurssitiedot, step3, step4 ja step5

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -8,43 +8,51 @@ const Part = (props) => (
 
 const Content = (props) => (
   <>
-    <Part title={props.contentObject.p1} exercises={props.contentObject.e1} />
-    <Part title={props.contentObject.p2} exercises={props.contentObject.e2} />
-    <Part title={props.contentObject.p3} exercises={props.contentObject.e3} />
+    {props.parts.map((part, index) => (
+    <Part 
+      key={index}
+      title={part.name}
+      exercises={part.exercises}
+    />
+    ))}
   </>
 );
 
-const Total = (props) => (
+const Total = (props) => {
+  let total = 0;
+  props.parts.forEach(part => total += part.exercises)
+
+  return (
   <p>
     Number of exercises{" "}
-    {props.totalObject.e1 + props.totalObject.e2 + props.totalObject.e3}
+    {total}
   </p>
-);
+)};
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-
-  const contentObject = {
-    p1: part1,
-    e1: exercises1,
-    p2: part2,
-    e2: exercises2,
-    p3: part3,
-    e3: exercises3,
-  };
-  const totalObject = { e1: exercises1, e2: exercises2, e3: exercises3 };
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-      <Header course={course} />
-      <Content contentObject={contentObject} />
-      <Total totalObject={totalObject} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 };
